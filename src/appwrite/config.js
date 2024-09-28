94cbdc2876e8ae1caae53dc70f49b798044cb380
dengrui <74555154+PAINKILLER0504@users.noreply.github.com>
2024-09-28 16:42:34 +0800

    Update config.js

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -2,18 +2,15 @@
 import { Client, Databases } from "appwrite";
  
 const client = new Client()
-    // .setEndpoint(import.meta.env.VITE_ENDPOINT)
-    .setEndpoint('https://cloud.appwrite.io/v1')
-    .setProject('66f155d5000c14bfd335');
+    .setEndpoint(import.meta.env.VITE_ENDPOINT)
+    .setProject(import.meta.env.VITE_PROJECT_ID);
  
 const databases = new Databases(client);
 
 const collections = [{
     name :"notes",
-    // id: import.meta.env.VITE_COLLECTION_NOTES_ID,
-    // dbID: import.meta.env.VITE_DATABASE_ID
-      id: '66f1566f002c6fe5856c',
-    dbID: '66f15646003ad9853da0'
+    id: import.meta.env.VITE_COLLECTION_NOTES_ID,
+    dbID: import.meta.env.VITE_DATABASE_ID
 }]
 
 export { client, databases ,collections}
